4e1e3c3d03e6df1fc5856ce1151f27cd18a9ff20
immanuel95 <immanueldaud@protonmail.com>
2026-01-12 12:00:36 +0700

    the `offsets` table refactor(bufferline): simplify configuration
    Remove the `offsets` table from the bufferline configuration. This
    simplifies the plugin's setup by removing the custom "File Explorer"
    text that was displayed for the NvimTree filetype.

--- lua/plugins/configs/bufferline.lua
+++ lua/plugins/configs/bufferline.lua
@@ -3,14 +3,5 @@ return {
     mode = "buffers",
     diagnostics = "nvim_lsp",
     themable = true,
-    offsets = {
-      {
-        filetype = "NvimTree",
-        highlight = "NvimTreeNormal",
-        text = "File Explorer",
-        text_align = "left",
-        separator = true,
-      },
-    },
   },
 }
